b66528bff5b2e2ee5acd4de838946846aaf096a2
Erwyn <r.erwyn@gmail.com>
2021-10-18 15:26:48 +0200

    TEST size contient testvaleurs

--- src/test/java/td1/AppTestGeneralisation.java
+++ src/test/java/td1/AppTestGeneralisation.java
@@ -17,21 +17,21 @@ public class AppTestGeneralisation {
         return new Noeud(new ArrayList<Arbre>());
     }
 
-    public static final Arbre arbre1() {
-        final Arbre f1 = new Feuille(1);
-        final Arbre f2 = new Feuille(2);
-        final Arbre n1 = new Noeud(List.of(f1,f2));
-        final Arbre f3 = new Feuille(3);
-        final Arbre n2 = new Noeud(List.of(n1,f3));
+    public static final Arbre<Integer> arbre1() {
+        final Arbre<Integer> f1 = new Feuille(1);
+        final Arbre<Integer> f2 = new Feuille(2);
+        final Arbre<Integer> n1 = new Noeud(List.of(f1,f2));
+        final Arbre<Integer> f3 = new Feuille(3);
+        final Arbre<Integer> n2 = new Noeud(List.of(n1,f3));
         return n2;
     }
 
     public static final Arbre arbre2() {
-        final Arbre f1 = new Feuille(2);
-        final Arbre f2 = new Feuille(3);
-        final Arbre n1 = new Noeud(List.of(f1,f2));
-        final Arbre f3 = new Feuille(1);
-        final Arbre n2 = new Noeud(List.of(n1,f3));
+        final Arbre<String> f1 = new Feuille(2);
+        final Arbre<String> f2 = new Feuille(3);
+        final Arbre<String> n1 = new Noeud(List.of(f1,f2));
+        final Arbre<String> f3 = new Feuille(1);
+        final Arbre<String> n2 = new Noeud(List.of(n1,f3));
         return n2;
     }
 
