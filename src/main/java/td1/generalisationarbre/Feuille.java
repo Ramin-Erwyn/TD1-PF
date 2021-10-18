2b943f96b06f7863d8b823ba2070854743206abf
Erwyn <r.erwyn@gmail.com>
2021-10-18 14:19:37 +0200

    generalisation de feuille et noeud

--- src/main/java/td1/generalisationarbre/Feuille.java
+++ src/main/java/td1/generalisationarbre/Feuille.java
@@ -1,4 +1,27 @@
 package td1.generalisationarbre;
 
-public class Feuille {
+import java.util.Set;
+
+public class Feuille <T> implements Arbre{
+    private final T valeur;
+
+    public Feuille(final T valeur) {
+        this.valeur = valeur;
+    }
+
+    @Override
+    public int taille() {
+        return 1;
+    }
+
+    @Override
+    public boolean contient(final Object val) {
+        return val.equals(valeur);
+    }
+
+    @Override
+    public Set<T> valeurs() {
+        return Set.of(valeur);
+    }
+
 }
--- src/main/java/td1/generalisationarbre/Noeud.java
+++ src/main/java/td1/generalisationarbre/Noeud.java
@@ -1,4 +1,40 @@
 package td1.generalisationarbre;
 
-public class Noeud {
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Noeud<T> implements Arbre{
+    private final List<Arbre> fils;
+
+    public Noeud(final List<Arbre> fils) {
+        this.fils = fils;
+    }
+
+    @Override
+    public int taille() {
+        int rtr = 0;
+        for (final Arbre a : fils) {
+            rtr += a.taille();
+        }
+        return rtr;
+    }
+
+    @Override
+    public boolean contient(final Object val) {
+        boolean rtr = false;
+        for (final Arbre a : fils) {
+            if (a.contient(val)) return true;
+        }
+        return rtr;
+    }
+
+    @Override
+    public Set<T> valeurs() {
+        Set<T> rtr = new HashSet<>();
+        for (final Arbre a : fils) {
+            rtr.addAll(a.valeurs());
+        }
+        return rtr;
+    }
 }
